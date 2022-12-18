ecf2412fadfb0a957b01ed2e1fb919b2c2c764e5
SKY <onthsky@gmail.com>
2022-12-19 01:12:55 +0300

    New Entities has added to Core Layer

---
+++ Core/Entities/Concrete/Wallet.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Abstract;
+
+namespace Core.Entities.Concrete
+{
+    public class Wallet : IEntity
+    {
+        public int WalletId { get; set; }
+        public byte[] UserId { get; set; }
+        public int Amount { get; set; }
+    }
+}
