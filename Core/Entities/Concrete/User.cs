cc83ef2f686e08d1d3dc6eaca8f04cd7e7a477f8
SKY <onthsky@gmail.com>
2022-12-19 03:06:30 +0300

    Entities has added

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -9,12 +9,13 @@ namespace Core.Entities.Concrete
 {
     public class User : IEntity
     {
-        public byte[] UserId { get; set; }
+        public int UserId { get;private set; }
+        public string UserAddress { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
-        public byte[] PaswordSalt { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public bool Status { get; set; }
     }
 }
--- Core/Entities/Concrete/Wallet.cs
+++ Core/Entities/Concrete/Wallet.cs
@@ -5,7 +5,7 @@ namespace Core.Entities.Concrete
     public class Wallet : IEntity
     {
         public int WalletId { get; set; }
-        public byte[] UserId { get; set; }
-        public int Amount { get; set; }
+        public string UserAddress { get; set; }
+        public int Balance { get; set; }
     }
 }
