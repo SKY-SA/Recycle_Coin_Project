bc951f7d30d059e9b9e6795fff22da8f916e22ad
SKY <onthsky@gmail.com>
2022-12-19 03:06:24 +0300

    Business codes has added

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Entities.DTOs;
+
+namespace Business.Abstract
+{
+    public interface IAuthService
+    {
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+        IResult UserExists(string email);
+        //<AccessToken> CreateAccessToken(User user);
+    }
+    //IDataResult<List<User>> GetAll();
+    //IDataResult<User> GetByEmail(string email);
+
+    //IResult Add(User user);
+    //IResult Update(User user);
+    //IResult Delete(User user);
+}
