a149bd2791c93fffe34e119f23b24012bd60df31
SKY <onthsky@gmail.com>
2022-12-19 03:04:39 +0300

    Constants has added

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string UserAlreadyExists = "User already exist !";
+
+        public static string SuccessfulLogin = "Login has done Successfully ";
+
+        public static string PasswordError = "Password is Wrong";
+
+        public static string UserNotFound = "User Not Found !";
+
+        public static string UserRegistered = "User has registered Successfully";
+    }
+}
